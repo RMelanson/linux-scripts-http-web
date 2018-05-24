dae623a76e9e6e5e162d7d12f5b21f543de97a46
root <root@ip-172-31-8-48.ca-central-1.compute.internal>
2018-05-24 16:17:05 +0000

    WIP

---
+++ webBootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+webCurrDir=$PWD
+
+#Set Cloning Properties
+pkg=Web
+gitRepo="linux-scripts-apps-web.git"
+installDir="/tmp/scripts/apps/WEB"
+if [ -f ~/.ssh/gitHub.key ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
+fi
+
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+
+exit 1
+
+# Setup $pkg
+cd $installDir
+. ./setup.sh
+
+cd $webCurrDir
