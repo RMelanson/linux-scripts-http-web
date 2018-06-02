8e772a6dd825103a2cf4defdfe2c3e28106f986c
jasonantao <jasantao99@gmail.com>
2018-06-02 14:57:55 -0400

    Update webBootstrap.sh

--- webBootstrap.sh
+++ webBootstrap.sh
@@ -12,20 +12,14 @@ yum update -y
 #INSTALL GIT
 yum install git -y
 
-if [ -z "$1" ]; then
-   branch=master
-else
-  branch=$1
-fi
-
 #Set Cloning Properties
 pkg=Web
 gitRepo="linux-scripts-apps-web.git"
 installDir="/tmp/scripts/apps/WEB"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone -b $branch git@github.com:RMelanson/"
+   clone="git clone git@github.com:RMelanson/"
 else
-   clone="git clone -b $branch https://github.com/RMelanson/"
+   clone="git clone https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
