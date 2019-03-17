f081a0bf362fccde3cfe8b10c6b198699cf72d0a
Robin Melanson <remcogold@gmail.com>
2019-03-17 01:08:48 -0400

    Update webBootstrap.sh

--- webBootstrap.sh
+++ webBootstrap.sh
@@ -13,7 +13,9 @@ yum update -y
 yum install git -y
 
 # SETUP ENVIRONMENT AND PARAMETERS
-. ./env/setEnv.sh
+pkg=HTTP_WEB
+gitRepo="linux-scripts-http-web.git"
+installDir="/tmp/scripts/apps/$pkg"
 
 if [ -f ~/.ssh/gitHub.key ]; then
    clone="git clone -git@github.com:RMelanson/"
