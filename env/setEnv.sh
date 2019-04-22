ba071f967ee386cbb90c362ab6b0a5ab3484dacb
Robin Melanson <remcogold@gmail.com>
2019-04-22 12:42:12 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # HTTP WEB CONFIGURATION PARAMETERS
-pkg=HTTP_WEB
+
 bootstrap="webBootstrap.sh"
 
 gitRepo="linux-scripts-http-web.git"
-installDir="/tmp/scripts/apps/$pkg"
+
+pkg=HTTP_WEB
+scriptType="apps"
+parentDir="/tmp/scripts/$scriptType/"
+installDir="$parentDir/$pkg"
