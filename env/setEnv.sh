58e4ccfa9f537d33d26a8555022d47ba9191e636
Robin Melanson <robin.e.melanson@gmail.com>
2019-07-31 00:32:00 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -10,3 +10,12 @@ pkg=HTTP_WEB
 scriptType="apps"
 parentDir="/tmp/scripts/$scriptType/"
 installDir="$parentDir/$pkg"
+
+pkgOwner=ec2-user
+
+echo Setting External Args
+echo These Arguments Overwrite Default Argument Settings
+for arg in "$@"; do
+  echo setArgs EXECUTING: export $arg
+  export $arg
+done
